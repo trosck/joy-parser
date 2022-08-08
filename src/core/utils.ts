4fe376a374046a26fd02ed463f016ff0a67a730f
Sergey Belov <kort.dll@gmail.com>
2022-08-08 19:29:28 +0300

    refactoring makedirIfNotExist function

--- src/core/utils.ts
+++ src/core/utils.ts
@@ -22,21 +22,10 @@ const fixPath = (path: string) => path?.replace(/\s/, '_')?.toLowerCase()
 const getImageId = (imagePath: string) => imagePath.match(/(\d+.[\w]+)$/)?.[1]
 
 const makedirIfNotExist = async (targetPath: string) => {
-  const directories = targetPath.split('/')
-
-  const existedDirectories: string[] = []
-  for (const directory of directories) {
-
-    const directoryPath = path.resolve(
-      existedDirectories.join(path.delimiter),
-      directory
-    )
-
-    try {
-      await access(directoryPath, fs.constants.W_OK)
-    } catch (e) {
-      await mkdir(directoryPath)
-    }
+  try {
+    await access(targetPath, fs.constants.W_OK)
+  } catch (e) {
+    await mkdir(targetPath)
   }
 }
 
